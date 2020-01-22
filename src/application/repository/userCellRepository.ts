d00e67da849183304fcf4452c94756c4a079077f
ysk8.parchase@gmail.com <ysk8.parchase@gmail.com>
2020-01-22 20:15:20 +0900

    userCellの初期化漏れを修正

--- src/application/repository/userCellRepository.ts
+++ src/application/repository/userCellRepository.ts
@@ -2,7 +2,7 @@ import CellPosition from '@/business/valueobject/cellPosition';
 import UserCell from '../entity/userCell';
 import CellNotFoundError from '@/business/cellNotFoundError';
 export default class UserCellRepository {
-  public static userCells: UserCell[];
+  public static userCells: UserCell[] = [];
   public static push(userCell: UserCell) {
     this.userCells.push(userCell);
   }
