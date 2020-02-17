0e691555c6c9e1c003b28431d06b7a3ed1a51ae7
ysk8.parchase@gmail.com <ysk8.parchase@gmail.com>
2020-02-17 11:26:26 +0900

    セルのサイズを算出する際に考慮すべきWindowの高さの調整値。

--- src/application/valueObject/cellStyle.ts
+++ src/application/valueObject/cellStyle.ts
@@ -4,12 +4,21 @@ import WindowWidth from './windowWidth';
 
 /** Cellのスタイルを定義するクラス。主にDisplaySizeによって動的に変化するスタイルを作る。 */
 export default class CellStyle {
+  /**
+   * セルのサイズを算出する際に考慮すべきWindowの高さの調整値。
+   * ヘッダー、フッター、コントローラを考慮した値を差し引いている。
+   */
+  private static readonly ADJUSTMENT_HEIGHT = 230;
+
   public static create(
     gameSize: GameSize,
     windowHeight: WindowHeight,
     windowWidth: WindowWidth
   ): CellStyle {
-    const baseSize = Math.min(windowHeight.value, windowWidth.value);
+    const baseSize = Math.min(
+      windowHeight.value - CellStyle.ADJUSTMENT_HEIGHT,
+      windowWidth.value
+    );
     return new CellStyle(gameSize, baseSize);
   }
   private constructor(private gameSize: GameSize, private baseSize: number) {}
