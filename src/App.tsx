12ace6d5951240896db07775f046f85e038a0015
ysk8hori <ysk8.hori@gmail.com>
2022-03-14 23:43:26 +0900

    numberplace-generator を動かせるようにする

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,47 @@
 import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { generateGame } from '@ysk8hori/numberplace-generator';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [puzzle, correct] = generateGame({ height: 3, width: 3 });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount(count => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(9, 20px)',
+        }}
+      >
+        {puzzle.cells.map(cell => {
+          return (
+            <div
+              key={`${cell.pos[0]}-${cell.pos[1]}`}
+              style={{ border: '1px solid black' }}
+            >
+              {cell.answer ?? ' '}
+            </div>
+          );
+        })}
+      </div>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(9, 20px)',
+        }}
+      >
+        {correct.cells.map(cell => {
+          return (
+            <div
+              key={`${cell.pos[0]}-${cell.pos[1]}`}
+              style={{ border: '1px solid black' }}
+            >
+              {cell.answer ?? ' '}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
