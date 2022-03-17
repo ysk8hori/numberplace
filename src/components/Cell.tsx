4c69b0e1338c60da62bbcd9d1e177663c3a4a7b4
ysk8hori <ysk8.hori@gmail.com>
2022-03-17 22:57:53 +0900

    chore: Cell コメントを追加

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
-export default function Cell({ answer }: { answer?: string }) {
+/**
+ * マス目１つを表すコンポーネント。以下の特徴を持つ。
+ *
+ * - answer を真ん中に表示する
+ * - 正方形
+ * - 自分の大きさに合わせたフォントのサイズで表示する
+ */
+export default function Cell({
+  answer,
+}: {
+  /** そのセルの答え。未回答ならば undefined */
+  answer: string | undefined;
+}) {
   return (
     <div className="box-border border-solid border aspect-square flex justify-center items-center">
       {answer}
