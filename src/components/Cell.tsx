60b6a1ce5096e2b7d03624f9d7e558d1149d7b9e
ysk8hori <ysk8.hori@gmail.com>
2022-03-17 22:46:05 +0900

    feat: Cell を正方形にし答えを真ん中に表示する

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Cell({ answer }: { answer?: string }) {
-  return <div className="box-border border-solid border">{answer}</div>;
+  return (
+    <div className="box-border border-solid border aspect-square flex justify-center items-center">
+      {answer}
+    </div>
+  );
 }
