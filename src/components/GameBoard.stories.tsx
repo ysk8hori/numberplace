ef1cb1eda2b7c9bb5bc9afe40dc65fcaf6bd7653
ysk8hori <ysk8.hori@gmail.com>
2022-03-20 14:04:35 +0900

    test: 共通で定義したパズルをテストで使用する

--- src/components/GameBoard.stories.tsx
+++ src/components/GameBoard.stories.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-
 import GameBoard from './GameBoard';
-import { BlockSize, Game } from '@ysk8hori/numberplace-generator';
-
-const blockSize: BlockSize = { height: 2, width: 3 };
-const puzzle: Game = {
-  ...JSON.parse(
-    '{"cells":[{"pos":[0,0]},{"pos":[1,0],"answer":"2"},{"pos":[2,0]},{"pos":[3,0],"answer":"4"},{"pos":[4,0],"answer":"3"},{"pos":[5,0],"answer":"5"},{"pos":[0,1],"answer":"4"},{"pos":[1,1]},{"pos":[2,1]},{"pos":[3,1]},{"pos":[4,1]},{"pos":[5,1],"answer":"1"},{"pos":[0,2],"answer":"3"},{"pos":[1,2]},{"pos":[2,2]},{"pos":[3,2],"answer":"2"},{"pos":[4,2]},{"pos":[5,2]},{"pos":[0,3]},{"pos":[1,3],"answer":"1"},{"pos":[2,3],"answer":"6"},{"pos":[3,3]},{"pos":[4,3]},{"pos":[5,3]},{"pos":[0,4]},{"pos":[1,4]},{"pos":[2,4],"answer":"2"},{"pos":[3,4]},{"pos":[4,4]},{"pos":[5,4],"answer":"4"},{"pos":[0,5]},{"pos":[1,5],"answer":"6"},{"pos":[2,5],"answer":"4"},{"pos":[3,5]},{"pos":[4,5],"answer":"5"},{"pos":[5,5]}]}',
-  ),
-  toString: () => ` ,2, ,4,3,5
-  4, , , , ,1
-  3, , ,2, , 
-   ,1,6, , , 
-   , ,2, , ,4
-   ,6,4, ,5, ,`,
-};
+import {
+  blockSize_2_3 as blockSize,
+  puzzle_2_3 as puzzle,
+} from '../utils/test-utils';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
--- src/components/GameBoard.test.tsx
+++ src/components/GameBoard.test.tsx
@@ -1,37 +1,14 @@
 import '@testing-library/jest-dom';
 import React from 'react';
 import { describe, test, fn, expect } from 'vitest';
-import { render, screen, userEvent } from '../utils/test-utils';
+import {
+  render,
+  screen,
+  userEvent,
+  puzzle_2_2 as puzzle,
+  blockSize_2_2 as blockSize,
+} from '../utils/test-utils';
 import GameBoard from './GameBoard';
-import { BlockSize, Game } from '@ysk8hori/numberplace-generator';
-
-const blockSize: BlockSize = { height: 2, width: 2 };
-
-const puzzle: Game = {
-  ...JSON.parse(
-    '{"cells":[{"pos":[0,0]},{"pos":[1,0],"answer":"4"},{"pos":[2,0]},{"pos":[3,0],"answer":"2"},{"pos":[0,1],"answer":"2"},{"pos":[1,1]},{"pos":[2,1],"answer":"4"},{"pos":[3,1]},{"pos":[0,2],"answer":"3"},{"pos":[1,2]},{"pos":[2,2]},{"pos":[3,2],"answer":"4"},{"pos":[0,3]},{"pos":[1,3]},{"pos":[2,3],"answer":"2"},{"pos":[3,3]}]}',
-  ),
-  toString: () => ` ,4, ,2
-2, ,4, 
-3, , ,4
- , ,2, `,
-};
-
-const blockSize_2_3: BlockSize = {
-  height: 2,
-  width: 3,
-};
-const puzzle_2_3: Game = {
-  ...JSON.parse(
-    '{"cells":[{"pos":[0,0]},{"pos":[1,0],"answer":"2"},{"pos":[2,0]},{"pos":[3,0],"answer":"4"},{"pos":[4,0],"answer":"3"},{"pos":[5,0],"answer":"5"},{"pos":[0,1],"answer":"4"},{"pos":[1,1]},{"pos":[2,1]},{"pos":[3,1]},{"pos":[4,1]},{"pos":[5,1],"answer":"1"},{"pos":[0,2],"answer":"3"},{"pos":[1,2]},{"pos":[2,2]},{"pos":[3,2],"answer":"2"},{"pos":[4,2]},{"pos":[5,2]},{"pos":[0,3]},{"pos":[1,3],"answer":"1"},{"pos":[2,3],"answer":"6"},{"pos":[3,3]},{"pos":[4,3]},{"pos":[5,3]},{"pos":[0,4]},{"pos":[1,4]},{"pos":[2,4],"answer":"2"},{"pos":[3,4]},{"pos":[4,4]},{"pos":[5,4],"answer":"4"},{"pos":[0,5]},{"pos":[1,5],"answer":"6"},{"pos":[2,5],"answer":"4"},{"pos":[3,5]},{"pos":[4,5],"answer":"5"},{"pos":[5,5]}]}',
-  ),
-  toString: () => ` ,2, ,4,3,5
-  4, , , , ,1
-  3, , ,2, , 
-   ,1,6, , , 
-   , ,2, , ,4
-   ,6,4, ,5, ,`,
-};
 
 describe('GameBoard', () => {
   test('GameBoard は 全ての cell を答えとともに表示する', () => {
