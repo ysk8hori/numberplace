9d2d05804032a244c8ff6fb53795b514e84ffeec
ysk8hori <ysk8.hori@gmail.com>
2022-03-20 13:57:50 +0900

    test: 共通で参照可能なパズルを定義

--- src/utils/test-utils.ts
+++ src/utils/test-utils.ts
@@ -11,3 +11,32 @@ export * from '@testing-library/react';
 export { default as userEvent } from '@testing-library/user-event';
 // override render export
 export { customRender as render };
+
+import { BlockSize, Game } from '@ysk8hori/numberplace-generator';
+
+export const blockSize_2_2: BlockSize = { height: 2, width: 2 };
+export const puzzle_2_2: Game = {
+  ...JSON.parse(
+    '{"cells":[{"pos":[0,0]},{"pos":[1,0],"answer":"4"},{"pos":[2,0]},{"pos":[3,0],"answer":"2"},{"pos":[0,1],"answer":"2"},{"pos":[1,1]},{"pos":[2,1],"answer":"4"},{"pos":[3,1]},{"pos":[0,2],"answer":"3"},{"pos":[1,2]},{"pos":[2,2]},{"pos":[3,2],"answer":"4"},{"pos":[0,3]},{"pos":[1,3]},{"pos":[2,3],"answer":"2"},{"pos":[3,3]}]}',
+  ),
+  toString: () => ` ,4, ,2
+2, ,4, 
+3, , ,4
+ , ,2, `,
+};
+
+export const blockSize_2_3: BlockSize = {
+  height: 2,
+  width: 3,
+};
+export const puzzle_2_3: Game = {
+  ...JSON.parse(
+    '{"cells":[{"pos":[0,0]},{"pos":[1,0],"answer":"2"},{"pos":[2,0]},{"pos":[3,0],"answer":"4"},{"pos":[4,0],"answer":"3"},{"pos":[5,0],"answer":"5"},{"pos":[0,1],"answer":"4"},{"pos":[1,1]},{"pos":[2,1]},{"pos":[3,1]},{"pos":[4,1]},{"pos":[5,1],"answer":"1"},{"pos":[0,2],"answer":"3"},{"pos":[1,2]},{"pos":[2,2]},{"pos":[3,2],"answer":"2"},{"pos":[4,2]},{"pos":[5,2]},{"pos":[0,3]},{"pos":[1,3],"answer":"1"},{"pos":[2,3],"answer":"6"},{"pos":[3,3]},{"pos":[4,3]},{"pos":[5,3]},{"pos":[0,4]},{"pos":[1,4]},{"pos":[2,4],"answer":"2"},{"pos":[3,4]},{"pos":[4,4]},{"pos":[5,4],"answer":"4"},{"pos":[0,5]},{"pos":[1,5],"answer":"6"},{"pos":[2,5],"answer":"4"},{"pos":[3,5]},{"pos":[4,5],"answer":"5"},{"pos":[5,5]}]}',
+  ),
+  toString: () => ` ,2, ,4,3,5
+  4, , , , ,1
+  3, , ,2, , 
+   ,1,6, , , 
+   , ,2, , ,4
+   ,6,4, ,5, ,`,
+};
