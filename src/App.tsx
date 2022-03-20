5b9c7ef82dbee298b763e2f028aee595c51ea06d
ysk8hori <ysk8.hori@gmail.com>
2022-03-20 23:08:00 +0900

    chore: App.tsx で GameContainer を使用する

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css';
 import { generateGame } from '@ysk8hori/numberplace-generator';
-import GameBoard from './components/GameBoard';
+import GameContainer from './containers/GameContainer';
 
 function App() {
   const blockSize = { height: 2, width: 2 };
   const [puzzle, correct] = generateGame(blockSize);
   console.log(puzzle.toString());
   console.log(JSON.stringify(puzzle));
-  return <GameBoard puzzle={puzzle} blockSize={blockSize} />;
+  return <GameContainer puzzle={puzzle} blockSize={blockSize} />;
 }
 
 export default App;
