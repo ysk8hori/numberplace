fd0694d7c49fa125a536c0c3ceda642beb456ff9
ysk8hori <ysk8.hori@gmail.com>
2022-03-26 23:22:56 +0900

    feat: モーダルの「いいえ」ボタンを押下するとモーダルが閉じる

---
+++ src/components/Verifying.test.tsx
@@ -0,0 +1,18 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { test, fn, expect } from 'vitest';
+import { render, screen, userEvent } from '../utils/test-utils';
+import Verifying from './Verifying';
+
+test('ボタンクリックでこたえあわせ確認モーダルを表示する', async () => {
+  render(<Verifying />);
+  userEvent.click(screen.getByRole('button', { name: 'こたえあわせ' }));
+  expect(await screen.findByRole('dialog', { name: '答え合わせの確認' }));
+});
+
+test('モーダルの「いいえ」ボタンを押下するとモーダルが閉じる', async () => {
+  render(<Verifying />);
+  userEvent.click(screen.getByRole('button', { name: 'こたえあわせ' }));
+  expect(await screen.findByRole('dialog', { name: '答え合わせの確認' }));
+  userEvent.click(screen.getByRole('button', { name: 'いいえ' }));
+});
--- src/components/Verifying.tsx
+++ src/components/Verifying.tsx
@@ -1,22 +1,32 @@
-import { useCallback, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import Modal from './atoms/Modal';
 import NeumorphismButton from './atoms/NeumorphismButton';
+import Spacer from './atoms/Spacer';
 
 /**
- * こたえあわせボタンとダイアログ
+ * こたえあわせボタンとモーダル
  *
- * - 「こたえあわせ」ボタンを押下したら答え合わせするかどうかの確認ダイアログを出す
+ * - 「こたえあわせ」ボタンを押下したら答え合わせするかどうかの確認モーダルを出す
+ * - モーダルの「いいえ」ボタンを押下するとモーダルが閉じる
  */
 export default function Verifying() {
   const [isOpen, setOpenState] = useState(false);
   const open = useCallback(() => setOpenState(true), [setOpenState]);
+  const close = useCallback(() => setOpenState(false), [setOpenState]);
   return (
     <>
       <NeumorphismButton onClick={() => open()} className="p-4 rounded-full ">
         こたえあわせ
       </NeumorphismButton>
       <Modal isOpen={isOpen} contentLabel="答え合わせの確認">
-        答え合わせしますか？
+        <p>答え合わせしますか？</p>
+        <Spacer />
+        <NeumorphismButton
+          onClick={() => close()}
+          className="p-2 rounded-full "
+        >
+          いいえ
+        </NeumorphismButton>
       </Modal>
     </>
   );
