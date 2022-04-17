b795759576f1ba6d230d2851c34dacea02081924
ysk8hori <ysk8.hori@gmail.com>
2022-04-18 00:30:41 +0900

    feat: cancel してメニューに戻れるようにした

--- src/containers/GenerateGameContainer.tsx
+++ src/containers/GenerateGameContainer.tsx
@@ -1,9 +1,11 @@
 import GameContainer from './GameContainer';
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 import useGenerateGame from '../useGenerateGame';
 import { BlockSize } from '@ysk8hori/numberplace-generator';
 import { Difficulty } from '../utils/difficulty';
 import SelfBuildingSquareSpinner from '../components/atoms/SelfBuildingSquareSpinner';
+import NeumorphismButton from '../components/atoms/NeumorphismButton';
+import styled from 'styled-components';
 
 function GenerateGameContainer({
   blockSize,
@@ -16,15 +18,27 @@ function GenerateGameContainer({
   onChangeSize?: () => void;
 }) {
   const [count, forceUpdate] = useReducer((x: number) => x + 1, 0);
+  const [showCancel, setShowCancel] = useState(false);
+  setTimeout(() => setShowCancel(true), 3000);
 
   const result = useGenerateGame({ blockSize, count, difficulty });
   if (result.isGenerating) {
     return (
-      <div className="flex flex-col justify-center items-center">
+      <div className="max-w-lg mx-auto flex flex-col justify-center items-center gap-5">
         <SelfBuildingSquareSpinner />
-        <div>
-          <button onClick={result.cancel}>cancel</button>
-        </div>
+        <HiddenBox className={showCancel ? 'visible' : undefined}>
+          <NeumorphismButton
+            onClick={() => (result.cancel(), onChangeSize?.())}
+            className="rounded-full px-5 py-1"
+          >
+            キャンセル
+          </NeumorphismButton>
+        </HiddenBox>
+        <HiddenBox className={showCancel ? 'visible' : undefined}>
+          <p>
+            大きいサイズのゲームはゲームの生成に時間がかかる場合があります。ゲームが生成されない場合はキャンセルし、他の大きさで遊んでください。
+          </p>
+        </HiddenBox>
       </div>
     );
   }
@@ -41,5 +55,16 @@ function GenerateGameContainer({
     </div>
   );
 }
+
+const HiddenBox = styled.div`
+  visibility: hidden;
+  opacity: 0;
+  transition-property: opacity visibility;
+  transition-duration: 0.5s;
+  &.visible {
+    visibility: visible;
+    opacity: 1;
+  }
+`;
 
 export default GenerateGameContainer;
