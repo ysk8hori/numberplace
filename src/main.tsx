275ff074656ad9d4250d1ddf7076d974cf0d1883
ysk8hori <ysk8.hori@gmail.com>
2022-04-27 22:35:54 +0900

    chore: setup sentry

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 import ReactModal from 'react-modal';
 import { registerSW } from 'virtual:pwa-register';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: 'https://d37eba4a04cc41b1b0762539d7d23409@o1222276.ingest.sentry.io/6366032',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 registerSW();
 
