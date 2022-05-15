ed472231b9a5a98a821817c2a52c01761488c2d7
ysk8hori <ysk8.hori@gmail.com>
2022-05-16 08:18:14 +0900

    不要なimportを削除

--- src/components/game/input-panel/ToggleMemoButton.tsx
+++ src/components/game/input-panel/ToggleMemoButton.tsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useRef, useState } from 'react';
+import React from 'react';
 import NeumorphismToggleButton from '../../atoms/NeumorphismToggleButton';
 import { TiPencil } from 'react-icons/ti';
 import clsx from 'clsx';
