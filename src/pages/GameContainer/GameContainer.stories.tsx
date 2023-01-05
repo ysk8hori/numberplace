f89de58def3743ae5cf5ad4b87ba46ba4bb637a1
ysk8hori <ysk8.hori@gmail.com>
2023-01-05 11:14:24 +0900

    GameContainer のストーリーを修正

--- src/pages/GameContainer/GameContainer.stories.tsx
+++ src/pages/GameContainer/GameContainer.stories.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  ComponentStory,
-  ComponentMeta,
-  ComponentStoryObj,
-} from '@storybook/react';
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
 import { within, userEvent } from '@storybook/testing-library';
 import GameContainer from '.';
 import {
@@ -22,7 +18,7 @@ import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import ReactModal from 'react-modal';
 import { expect } from '@storybook/jest';
 import { useSetRecoilState } from 'recoil';
-import { atomOfAnswerImageVariant } from '../../atoms';
+import { atomOfAnswerImageVariant, atomOfGame } from '../../atoms';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -39,84 +35,86 @@ export default {
       setIconMode('num');
       return <Story />;
     },
+    Story => {
+      const setGame = useSetRecoilState(atomOfGame);
+      setGame({
+        puzzle,
+        solved: solved_2_3,
+        blockSize,
+        cross: false,
+        hyper: false,
+      });
+      return <Story />;
+    },
   ],
 } as ComponentMeta<typeof GameContainer>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof GameContainer> = args => (
-  <GameContainer {...args} />
-);
-Template.args = {
-  // puzzle,
-  // solved: solved_2_3,
-  // blockSize,
+export const Primary: ComponentStoryObj<typeof GameContainer> = {};
+
+export const IconMode: ComponentStoryObj<typeof GameContainer> = {
+  decorators: [
+    Story => {
+      const setIconMode = useSetRecoilState(atomOfAnswerImageVariant);
+      setIconMode('asobi');
+      return <Story />;
+    },
+  ],
 };
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = Template.args;
+export const Primary_2_2: ComponentStoryObj<typeof GameContainer> = {
+  decorators: [
+    Story => {
+      const setGame = useSetRecoilState(atomOfGame);
+      setGame({
+        puzzle: puzzle_2_2,
+        solved: solved_2_2,
+        blockSize: blockSize_2_2,
+        cross: false,
+        hyper: false,
+      });
+      return <Story />;
+    },
+  ],
+};
 
-export const IconMode = Template.bind({});
-IconMode.args = Template.args;
-IconMode.decorators = [
-  Story => {
-    const setIconMode = useSetRecoilState(atomOfAnswerImageVariant);
-    setIconMode('asobi');
-    return <Story />;
+export const IPad: ComponentStoryObj<typeof GameContainer> = {
+  parameters: {
+    viewport: { defaultViewport: 'ipad' },
   },
-];
-
-export const Primary_2_2 = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary_2_2.args = {
-  // puzzle: puzzle_2_2,
-  // solved: solved_2_2,
-  // blockSize: blockSize_2_2,
 };
 
-export const IPad = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-IPad.args = Template.args;
-IPad.parameters = {
-  ...Template.parameters,
-  viewport: { defaultViewport: 'ipad' },
+export const Click2_3: ComponentStoryObj<typeof GameContainer> = {
+  name: 'セルをクリックすると選択中となる',
+  play: async ({ canvasElement }) => {
+    // Starts querying the component from its root element
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByTestId('2,3'));
+  },
 };
 
-export const Click2_3 = Template.bind({});
-Click2_3.args = Template.args;
-Click2_3.play = async ({ canvasElement }) => {
-  // Starts querying the component from its root element
-  const canvas = within(canvasElement);
-  await userEvent.click(canvas.getByTestId('2,3'));
+export const FillableByPanel: ComponentStoryObj<typeof GameContainer> = {
+  name: 'パネルのボタンから数字を入力できる',
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByTestId('2,0'));
+    await userEvent.click(canvas.getByRole('button', { name: '1' }));
+  },
 };
-Click2_3.storyName = 'セルをクリックすると選択中となる';
 
-export const FillableByPanel = Template.bind({});
-FillableByPanel.args = Template.args;
-FillableByPanel.play = async ({ canvasElement }) => {
-  // Starts querying the component from its root element
-  const canvas = within(canvasElement);
-  await userEvent.click(canvas.getByTestId('2,0'));
-  await userEvent.click(canvas.getByRole('button', { name: '1' }));
+export const ClearModal: ComponentStoryObj<typeof GameContainer> = {
+  name: 'クリア時にはクリアモーダルを表示する',
+  play: async ({ canvasElement }) => {
+    ReactModal.setAppElement(canvasElement);
+    // モーダルが canvasElement の外に描画されモーダル内の要素が取れないので body を canvas にしないと動かない
+    const canvas = within(canvasElement.parentElement!);
+    await resolve_2_3(canvas, { finish: true });
+    await expect(canvas.queryByText('クリア！')).not.toBeInTheDocument();
+    await userEvent.click(canvas.getByRole('button', { name: '答え合わせ' }));
+    await expect(canvas.getByText('クリア！')).toBeInTheDocument();
+  },
 };
-FillableByPanel.storyName = 'パネルのボタンから数字を入力できる';
-
-export const ClearModal = Template.bind({});
-
-ClearModal.args = Template.args;
-ClearModal.play = async ({ canvasElement }) => {
-  ReactModal.setAppElement(canvasElement);
-  // モーダルが canvasElement の外に描画されモーダル内の要素が取れないので body を canvas にしないと動かない
-  const canvas = within(canvasElement.parentElement!);
-  await resolve_2_3(canvas, { finish: true });
-  await expect(canvas.queryByText('クリア！')).not.toBeInTheDocument();
-  await userEvent.click(canvas.getByRole('button', { name: '答え合わせ' }));
-  await expect(canvas.getByText('クリア！')).toBeInTheDocument();
-};
-ClearModal.storyName = 'クリア時にはクリアモーダルを表示する';
 
 export const MistakeModal: ComponentStoryObj<typeof GameContainer> = {
-  args: Template.args,
   name: 'ミスが有る状態で答え合わせボタンを謳歌した場合はミステイクモーダルを表示する',
   play: async ({ canvasElement }) => {
     ReactModal.setAppElement(canvasElement);
@@ -131,38 +129,70 @@ export const MistakeModal: ComponentStoryObj<typeof GameContainer> = {
   },
 };
 
-export const Game_3_3 = Template.bind({});
-Game_3_3.args = {
-  // blockSize: blockSize_3_3,
-  // puzzle: puzzle_3_3,
-  // solved: solved_3_3,
+export const Game_3_3: ComponentStoryObj<typeof GameContainer> = {
+  name: '9x9の表示確認',
+  decorators: [
+    Story => {
+      const setGame = useSetRecoilState(atomOfGame);
+      setGame({
+        puzzle: puzzle_3_3,
+        solved: solved_3_3,
+        blockSize: blockSize_3_3,
+        cross: false,
+        hyper: false,
+      });
+      return <Story />;
+    },
+  ],
 };
-Game_3_3.storyName = '9x9の表示確認';
 
-export const Game_3_3_cross = Template.bind({});
-Game_3_3_cross.args = {
-  // blockSize: blockSize_3_3,
-  // puzzle: puzzle_3_3,
-  // solved: solved_3_3,
-  // cross: true,
+export const Game_3_3_cross: ComponentStoryObj<typeof GameContainer> = {
+  name: '9x9クロスの表示確認',
+  decorators: [
+    Story => {
+      const setGame = useSetRecoilState(atomOfGame);
+      setGame({
+        puzzle: puzzle_3_3,
+        solved: solved_3_3,
+        blockSize: blockSize_3_3,
+        cross: true,
+        hyper: false,
+      });
+      return <Story />;
+    },
+  ],
 };
-Game_3_3_cross.storyName = '9x9クロスの表示確認';
 
-export const Game_3_3_hyper = Template.bind({});
-Game_3_3_hyper.args = {
-  // blockSize: blockSize_3_3,
-  // puzzle: puzzle_3_3,
-  // solved: solved_3_3,
-  // hyper: true,
+export const Game_3_3_hyper: ComponentStoryObj<typeof GameContainer> = {
+  name: '9x9HYPERの表示確認',
+  decorators: [
+    Story => {
+      const setGame = useSetRecoilState(atomOfGame);
+      setGame({
+        puzzle: puzzle_3_3,
+        solved: solved_3_3,
+        blockSize: blockSize_3_3,
+        cross: false,
+        hyper: true,
+      });
+      return <Story />;
+    },
+  ],
 };
-Game_3_3_hyper.storyName = '9x9HYPERの表示確認';
 
-export const Game_3_3_hyper_cross = Template.bind({});
-Game_3_3_hyper_cross.args = {
-  // blockSize: blockSize_3_3,
-  // puzzle: puzzle_3_3,
-  // solved: solved_3_3,
-  // cross: true,
-  // hyper: true,
+export const Game_3_3_hyper_cross: ComponentStoryObj<typeof GameContainer> = {
+  name: '9x9HYPERxCROSSの表示確認',
+  decorators: [
+    Story => {
+      const setGame = useSetRecoilState(atomOfGame);
+      setGame({
+        puzzle: puzzle_3_3,
+        solved: solved_3_3,
+        blockSize: blockSize_3_3,
+        cross: true,
+        hyper: true,
+      });
+      return <Story />;
+    },
+  ],
 };
-Game_3_3_hyper_cross.storyName = '9x9HYPERxCROSSの表示確認';
