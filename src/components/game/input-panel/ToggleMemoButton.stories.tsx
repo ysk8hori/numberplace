142b04b102696a7e946835b3e13dd0047441b9ec
ysk8hori <ysk8.hori@gmail.com>
2023-11-06 21:01:51 +0900

    fix: メモボタンがハイライトされるよう修正

--- src/components/game/input-panel/ToggleMemoButton.stories.tsx
+++ src/components/game/input-panel/ToggleMemoButton.stories.tsx
@@ -2,9 +2,25 @@ import React from 'react';
 import { Meta, StoryObj } from '@storybook/react';
 
 import ToggleMemoButton from './ToggleMemoButton';
+import { userEvent, within } from '@storybook/testing-library';
 
 export default {
   component: ToggleMemoButton,
+  decorators: [
+    Story => (
+      <div className="w-20 h-20">
+        <Story />
+      </div>
+    ),
+  ],
 } as Meta<typeof ToggleMemoButton>;
 
 export const Default: StoryObj<typeof ToggleMemoButton> = {};
+export const Checked: StoryObj<typeof ToggleMemoButton> = {
+  name: 'クリックするとチェックが入る',
+  play: async ({ canvasElement }) => {
+    // Starts querying the component from its root element
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole('checkbox'));
+  },
+};
