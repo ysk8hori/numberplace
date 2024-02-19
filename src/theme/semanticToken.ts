1e084f0de465cf09921c777931a8f0e662f476b0
ysk8hori <ysk8.hori@gmail.com>
2024-02-19 22:40:49 +0900

    feat(button): ダークテーマ対応

--- src/theme/semanticToken.ts
+++ src/theme/semanticToken.ts
@@ -3,82 +3,164 @@ import colors from './colors';
 
 const defaultToken = {
   button: {
-    flat: {
-      normal: {
-        border_width: '0px',
-        border_color: 'none',
-        bg_color: colors.zinc[400],
-        color: colors.zinc[50],
+    light: {
+      flat: {
+        normal: {
+          border_width: '0px',
+          border_color: 'none',
+          bg_color: colors.zinc[400],
+          color: colors.zinc[50],
+        },
+        hover: {
+          border_width: '0px',
+          border_color: 'none',
+          bg_color: colors.zinc[500],
+          color: colors.zinc[50],
+        },
+        active: {
+          border_width: '0px',
+          border_color: 'none',
+          bg_color: colors.zinc[400],
+          color: colors.zinc[50],
+        },
+        disabled: {
+          border_width: '0px',
+          border_color: 'none',
+          bg_color: colors.zinc[300],
+          color: colors.zinc[50],
+        },
       },
-      hover: {
-        border_width: '0px',
-        border_color: 'none',
-        bg_color: colors.zinc[500],
-        color: colors.zinc[50],
+      outlined: {
+        normal: {
+          border_width: '1px',
+          border_color: colors.zinc[700],
+          bg_color: colors.transparent,
+          color: colors.zinc[700],
+        },
+        hover: {
+          border_width: '1px',
+          border_color: colors.zinc[700],
+          bg_color: `${colors.zinc[700]}20`,
+          color: colors.zinc[700],
+        },
+        active: {
+          border_width: '1px',
+          border_color: colors.zinc[700],
+          bg_color: colors.transparent,
+          color: colors.zinc[700],
+        },
+        disabled: {
+          border_width: '1px',
+          border_color: colors.zinc[400],
+          bg_color: colors.transparent,
+          color: colors.zinc[300],
+        },
       },
-      active: {
-        border_width: '0px',
-        border_color: 'none',
-        bg_color: colors.zinc[400],
-        color: colors.zinc[50],
-      },
-      disabled: {
-        border_width: '0px',
-        border_color: 'none',
-        bg_color: colors.zinc[300],
-        color: colors.zinc[50],
+      text: {
+        normal: {
+          border_width: '0px',
+          border_color: 'none',
+          bg_color: colors.transparent,
+          color: colors.zinc[700],
+        },
+        hover: {
+          border_width: '0px',
+          border_color: 'none',
+          bg_color: `${colors.zinc[700]}20`,
+          color: colors.zinc[700],
+        },
+        active: {
+          border_width: '0px',
+          border_color: 'none',
+          bg_color: `${colors.zinc[700]}10`,
+          color: colors.zinc[700],
+        },
+        disabled: {
+          border_width: '0px',
+          border_color: 'none',
+          bg_color: colors.transparent,
+          color: colors.zinc[300],
+        },
       },
     },
-    outlined: {
-      normal: {
-        border_width: '1px',
-        border_color: colors.zinc[700],
-        bg_color: colors.transparent,
-        color: colors.zinc[700],
+    dark: {
+      flat: {
+        normal: {
+          border_width: '0px',
+          border_color: 'none',
+          bg_color: colors.zinc[700],
+          color: colors.zinc[50],
+        },
+        hover: {
+          border_width: '0px',
+          border_color: 'none',
+          bg_color: colors.zinc[500],
+          color: colors.zinc[50],
+        },
+        active: {
+          border_width: '0px',
+          border_color: 'none',
+          bg_color: colors.zinc[700],
+          color: colors.zinc[50],
+        },
+        disabled: {
+          border_width: '0px',
+          border_color: 'none',
+          bg_color: colors.zinc[800],
+          color: colors.zinc[500],
+        },
       },
-      hover: {
-        border_width: '1px',
-        border_color: colors.zinc[700],
-        bg_color: `${colors.zinc[700]}20`,
-        color: colors.zinc[700],
+      outlined: {
+        normal: {
+          border_width: '1px',
+          border_color: colors.zinc[400],
+          bg_color: colors.transparent,
+          color: colors.zinc[300],
+        },
+        hover: {
+          border_width: '1px',
+          border_color: colors.zinc[300],
+          bg_color: `${colors.zinc[300]}20`,
+          color: colors.zinc[200],
+        },
+        active: {
+          border_width: '1px',
+          border_color: colors.zinc[300],
+          bg_color: `${colors.zinc[300]}10`,
+          color: colors.zinc[200],
+        },
+        disabled: {
+          border_width: '1px',
+          border_color: colors.zinc[700],
+          bg_color: colors.transparent,
+          color: colors.zinc[700],
+        },
       },
-      active: {
-        border_width: '1px',
-        border_color: colors.zinc[700],
-        bg_color: colors.transparent,
-        color: colors.zinc[700],
-      },
-      disabled: {
-        border_width: '1px',
-        border_color: colors.zinc[400],
-        bg_color: colors.transparent,
-        color: colors.zinc[300],
-      },
-    },
-    text: {
-      normal: {
-        border_width: '0px',
-        border_color: 'none',
-        bg_color: colors.transparent,
-        color: colors.zinc[700],
-      },
-      hover: {
-        border_width: '0px',
-        border_color: 'none',
-        bg_color: `${colors.zinc[700]}20`,
-        color: colors.zinc[700],
-      },
-      active: {
-        border_width: '0px',
-        border_color: 'none',
-        bg_color: `${colors.zinc[700]}10`,
-        color: colors.zinc[700],
-      },
-      disabled: {
-        border_width: '0px',
-        border_color: 'none',
-        bg_color: colors.transparent,
-        color: colors.zinc[300],
+      text: {
+        normal: {
+          border_width: '0px',
+          border_color: 'none',
+          bg_color: colors.transparent,
+          color: colors.zinc[300],
+        },
+        hover: {
+          border_width: '0px',
+          border_color: 'none',
+          bg_color: `${colors.zinc[300]}20`,
+          color: colors.zinc[300],
+        },
+        active: {
+          border_width: '0px',
+          border_color: 'none',
+          bg_color: `${colors.zinc[300]}10`,
+          color: colors.zinc[300],
+        },
+        disabled: {
+          border_width: '0px',
+          border_color: 'none',
+          bg_color: colors.transparent,
+          color: colors.zinc[700],
+        },
       },
     },
   },
