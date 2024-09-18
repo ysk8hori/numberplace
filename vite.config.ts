28d7e00ceb1fb56086892152443cdb8c6b69f12f
ysk8hori <ysk8.hori@gmail.com>
2024-09-18 22:06:54 +0900

    feat: 新しいジェネレータをhyperに適用

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext',
+  },
+  optimizeDeps: { esbuildOptions: { target: 'esnext' } },
   plugins: [
     react(),
     VitePWA({
