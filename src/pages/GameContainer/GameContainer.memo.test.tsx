b33f89108c3ae81429a533e2e1a6345fbcb860df
ysk8hori <ysk8.hori@gmail.com>
2024-09-28 19:55:04 +0900

    test: メモのテストを jotai に適応させる
    メモボタン単体のテストはあまり役に立たないので削除

--- src/pages/GameContainer/GameContainer.memo.test.tsx
+++ src/pages/GameContainer/GameContainer.memo.test.tsx
@@ -12,129 +12,136 @@ import {
   puzzle_2_3,
   blockSize_2_3,
   solved_2_3,
+  TestProvider,
 } from '../../utils/test-utils';
 import GameContainer from '.';
 import { atomOfGame, atomOfSolved } from '../../atoms';
 
-// function setup(size: '2_2' | '2_3') {
-//   const rendered = render(
-//     size === '2_2' ? (
-//       <>
-//         <RecoilServer
-//           node={atomOfGame}
-//           value={{
-//             puzzle: puzzle_2_2,
-//             blockSize: blockSize_2_2,
-//             hyper: false,
-//             cross: false,
-//           }}
-//         />
-//         <RecoilServer node={atomOfSolved} value={solved_2_2} />
-//         <GameContainer />
-//       </>
-//     ) : (
-//       <>
-//         <RecoilServer
-//           node={atomOfGame}
-//           value={{
-//             puzzle: puzzle_2_3,
-//             blockSize: blockSize_2_3,
-//             hyper: false,
-//             cross: false,
-//           }}
-//         />
-//         <RecoilServer node={atomOfSolved} value={solved_2_3} />
-//         <GameContainer />
-//       </>
-//     ),
-//   );
-//   ReactModal.setAppElement(rendered.container);
-// }
+function setup(size: '2_2' | '2_3') {
+  const rendered = render(
+    size === '2_2' ? (
+      <TestProvider
+        initialValues={[
+          [
+            atomOfGame,
+            {
+              puzzle: puzzle_2_2,
+              blockSize: blockSize_2_2,
+              hyper: false,
+              cross: false,
+            },
+          ],
+          [atomOfSolved, solved_2_2],
+        ]}
+      >
+        <GameContainer />
+      </TestProvider>
+    ) : (
+      <TestProvider
+        initialValues={[
+          [
+            atomOfGame,
+            {
+              puzzle: puzzle_2_3,
+              blockSize: blockSize_2_3,
+              hyper: false,
+              cross: false,
+            },
+          ],
+          [atomOfSolved, solved_2_3],
+        ]}
+      >
+        <GameContainer />
+      </TestProvider>
+    ),
+  );
+  ReactModal.setAppElement(rendered.container);
+}
 
-// test('メモモードで入力パネルから空欄セルにメモを記入できる', async () => {
-//   setup('2_2');
-//   expect(screen.getByTestId('0,0')).not.toHaveTextContent('1');
-//   expect(screen.queryByTestId('0,0-memo')).not.toBeInTheDocument();
-//   await userEvent.click(screen.getByTestId('0,0'));
-//   await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
-//   await userEvent.click(screen.getByRole('button', { name: '1' }));
-//   expect(screen.getByTestId('0,0-memo')).toHaveAttribute('data-memo', '1');
-// });
-// test('メモモードでキーボードから空欄セルにメモを記入できる', async () => {
-//   setup('2_2');
-//   expect(screen.getByTestId('0,0')).not.toHaveTextContent('1');
-//   expect(screen.queryByTestId('0,0-memo')).not.toBeInTheDocument();
-//   await userEvent.click(screen.getByTestId('0,0'));
-//   await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
-//   await userEvent.keyboard('1');
-//   expect(screen.getByTestId('0,0-memo')).toHaveAttribute('data-memo', '1');
-// });
-// test('メモモードでメモ済み数字と同じボタン押下でそのメモ数字を消す', async () => {
-//   setup('2_2');
-//   expect(screen.getByTestId('0,0')).not.toHaveTextContent('1');
-//   expect(screen.queryByTestId('0,0-memo')).not.toBeInTheDocument();
-//   await userEvent.click(screen.getByTestId('0,0'));
-//   await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
-//   await userEvent.click(screen.getByRole('button', { name: '1' }));
-//   await userEvent.click(screen.getByRole('button', { name: '2' }));
-//   expect(screen.getByTestId('0,0-memo')).toHaveAttribute('data-memo', '1,2');
-//   await userEvent.click(screen.getByRole('button', { name: '1' }));
-//   expect(screen.getByTestId('0,0-memo')).not.toHaveTextContent('1');
-//   expect(screen.getByTestId('0,0-memo')).toHaveAttribute('data-memo', '2');
-// });
-// test('メモモードで入力済みセル上書き', async () => {
-//   setup('2_2');
-//   expect(screen.getByTestId('0,0')).not.toHaveAttribute('data-answer');
-//   await userEvent.click(screen.getByTestId('0,0'));
-//   await userEvent.click(screen.getByRole('button', { name: '2' }));
-//   expect(screen.getByTestId('0,0')).toHaveAttribute('data-answer', '2');
-//   await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
-//   await userEvent.click(screen.getByRole('button', { name: '1' }));
-//   expect(screen.getByTestId('0,0')).not.toHaveAttribute('data-answer');
-//   expect(screen.getByTestId('0,0-memo')).toHaveAttribute('data-memo', '1');
-// });
-// test('メモ記入済みセルに通常入力で上書き', async () => {
-//   setup('2_2');
-//   expect(screen.getByTestId('0,0')).not.toHaveAttribute('data-answer');
-//   await userEvent.click(screen.getByTestId('0,0'));
-//   await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
-//   await userEvent.click(screen.getByRole('button', { name: '1' }));
-//   expect(screen.getByTestId('0,0-memo')).toHaveAttribute('data-memo', '1');
-//   await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
-//   await userEvent.click(screen.getByRole('button', { name: '2' }));
-//   expect(screen.getByTestId('0,0')).toHaveAttribute('data-answer', '2');
-// });
-// test('メモがある、かつメモモードで消去ボタンを押下するとメモをクリアする', async () => {
-//   setup('2_2');
-//   expect(screen.getByTestId('0,0')).not.toHaveAttribute('data-answer');
-//   await userEvent.click(screen.getByTestId('0,0'));
-//   await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
-//   await userEvent.click(screen.getByRole('button', { name: '1' }));
-//   await userEvent.click(screen.getByRole('button', { name: '2' }));
-//   expect(screen.getByTestId('0,0-memo')).toHaveAttribute('data-memo', '1,2');
-//   await userEvent.click(screen.getByRole('button', { name: '消す' }));
-//   expect(screen.queryByTestId('0,0-memo')).not.toBeInTheDocument();
-// });
-// test('メモがある、かつ通常モードで消去ボタンを押下するとメモをクリアする', async () => {
-//   setup('2_2');
-//   expect(screen.getByTestId('0,0')).not.toHaveAttribute('data-answer');
-//   await userEvent.click(screen.getByTestId('0,0'));
-//   await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
-//   await userEvent.click(screen.getByRole('button', { name: '1' }));
-//   await userEvent.click(screen.getByRole('button', { name: '2' }));
-//   await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
-//   expect(screen.getByTestId('0,0-memo')).toHaveAttribute('data-memo', '1,2');
-//   await userEvent.click(screen.getByRole('button', { name: '消す' }));
-//   expect(screen.queryByTestId('0,0-memo')).not.toBeInTheDocument();
-// });
+test('メモモードで入力パネルから空欄セルにメモを記入できる', async () => {
+  setup('2_2');
+  expect(screen.getByTestId('0,0')).not.toHaveTextContent('1');
+  expect(screen.queryByTestId('0,0-memo')).not.toBeInTheDocument();
+  await userEvent.click(screen.getByTestId('0,0'));
+  await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
+  await userEvent.click(screen.getByRole('button', { name: '1' }));
+  expect(screen.getByTestId('0,0-memo')).toHaveAttribute('data-memo', '1');
+});
+test('メモモードでキーボードから空欄セルにメモを記入できる', async () => {
+  setup('2_2');
+  expect(screen.getByTestId('0,0')).not.toHaveTextContent('1');
+  expect(screen.queryByTestId('0,0-memo')).not.toBeInTheDocument();
+  await userEvent.click(screen.getByTestId('0,0'));
+  await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
+  await userEvent.keyboard('1');
+  expect(screen.getByTestId('0,0-memo')).toHaveAttribute('data-memo', '1');
+});
+test('メモモードでメモ済み数字と同じボタン押下でそのメモ数字を消す', async () => {
+  setup('2_2');
+  expect(screen.getByTestId('0,0')).not.toHaveTextContent('1');
+  expect(screen.queryByTestId('0,0-memo')).not.toBeInTheDocument();
+  await userEvent.click(screen.getByTestId('0,0'));
+  await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
+  await userEvent.click(screen.getByRole('button', { name: '1' }));
+  await userEvent.click(screen.getByRole('button', { name: '2' }));
+  expect(screen.getByTestId('0,0-memo')).toHaveAttribute('data-memo', '1,2');
+  await userEvent.click(screen.getByRole('button', { name: '1' }));
+  expect(screen.getByTestId('0,0-memo')).not.toHaveTextContent('1');
+  expect(screen.getByTestId('0,0-memo')).toHaveAttribute('data-memo', '2');
+});
+test('メモモードで入力済みセル上書き', async () => {
+  setup('2_2');
+  expect(screen.getByTestId('0,0')).not.toHaveAttribute('data-answer');
+  await userEvent.click(screen.getByTestId('0,0'));
+  await userEvent.click(screen.getByRole('button', { name: '2' }));
+  expect(screen.getByTestId('0,0')).toHaveAttribute('data-answer', '2');
+  await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
+  await userEvent.click(screen.getByRole('button', { name: '1' }));
+  expect(screen.getByTestId('0,0')).not.toHaveAttribute('data-answer');
+  expect(screen.getByTestId('0,0-memo')).toHaveAttribute('data-memo', '1');
+});
+test('メモ記入済みセルに通常入力で上書き', async () => {
+  setup('2_2');
+  expect(screen.getByTestId('0,0')).not.toHaveAttribute('data-answer');
+  await userEvent.click(screen.getByTestId('0,0'));
+  await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
+  await userEvent.click(screen.getByRole('button', { name: '1' }));
+  expect(screen.getByTestId('0,0-memo')).toHaveAttribute('data-memo', '1');
+  await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
+  await userEvent.click(screen.getByRole('button', { name: '2' }));
+  expect(screen.getByTestId('0,0')).toHaveAttribute('data-answer', '2');
+});
+test('メモがある、かつメモモードで消去ボタンを押下するとメモをクリアする', async () => {
+  setup('2_2');
+  expect(screen.getByTestId('0,0')).not.toHaveAttribute('data-answer');
+  await userEvent.click(screen.getByTestId('0,0'));
+  await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
+  await userEvent.click(screen.getByRole('button', { name: '1' }));
+  await userEvent.click(screen.getByRole('button', { name: '2' }));
+  expect(screen.getByTestId('0,0-memo')).toHaveAttribute('data-memo', '1,2');
+  await userEvent.click(screen.getByRole('button', { name: '消す' }));
+  expect(screen.queryByTestId('0,0-memo')).not.toBeInTheDocument();
+});
+test('メモがある、かつ通常モードで消去ボタンを押下するとメモをクリアする', async () => {
+  setup('2_2');
+  expect(screen.getByTestId('0,0')).not.toHaveAttribute('data-answer');
+  await userEvent.click(screen.getByTestId('0,0'));
+  await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
+  await userEvent.click(screen.getByRole('button', { name: '1' }));
+  await userEvent.click(screen.getByRole('button', { name: '2' }));
+  await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
+  expect(screen.getByTestId('0,0-memo')).toHaveAttribute('data-memo', '1,2');
+  await userEvent.click(screen.getByRole('button', { name: '消す' }));
+  expect(screen.queryByTestId('0,0-memo')).not.toBeInTheDocument();
+});
 
 test('fix のセルはメモ入力ボタンでメモを記入できない', async () => {
-  // setup('2_2');
-  // expect(screen.getByTestId('1,0')).toHaveAttribute('data-answer', '4');
-  // expect(screen.getByTestId('1,0')).toHaveAttribute('data-fix');
-  // await userEvent.click(screen.getByTestId('1,0'));
-  // await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
-  // await userEvent.click(screen.getByRole('button', { name: '1' }));
-  // expect(screen.getByTestId('1,0')).toHaveAttribute('data-answer', '4');
-  // expect(screen.getByTestId('1,0')).toHaveAttribute('data-fix');
+  setup('2_2');
+  expect(screen.getByTestId('1,0')).toHaveAttribute('data-answer', '4');
+  expect(screen.getByTestId('1,0')).toHaveAttribute('data-fix');
+  await userEvent.click(screen.getByTestId('1,0'));
+  await userEvent.click(screen.getByRole('checkbox', { name: 'メモ' }));
+  await userEvent.click(screen.getByRole('button', { name: '1' }));
+  expect(screen.getByTestId('1,0')).toHaveAttribute('data-answer', '4');
+  expect(screen.getByTestId('1,0')).toHaveAttribute('data-fix');
 });
