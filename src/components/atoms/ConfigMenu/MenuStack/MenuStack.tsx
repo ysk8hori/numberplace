4811ccbbed6d881de2adbb79f18eb93dee18e5cf
ysk8hori <ysk8.hori@gmail.com>
2025-11-08 21:07:18 +0900

    refactor: ConfigMenuをpopover APIを使用した実装に変更

--- src/components/atoms/ConfigMenu/MenuStack/MenuStack.tsx
+++ src/components/atoms/ConfigMenu/MenuStack/MenuStack.tsx
@@ -1,129 +1,102 @@
-import clsx from 'clsx';
-import React, { memo, useCallback, useMemo } from 'react';
+import React, { memo, useCallback } from 'react';
 import { atomOfGame, atomOfInitial } from '../../../../atoms';
-import Button from '../../Button';
-import './MenuStack.scss';
+import styles from './MenuStack.module.scss';
 import { toURLSearchParam } from '../../../../utils/URLSearchParamConverter';
 import Quit from '../../../game/Quit';
 import { useAtom } from 'jotai';
 import { atomOfAnswerImageVariant } from '../../../../jotaiAtoms';
 
-export default function MenuStack({
-  isShow,
-  onSelected,
-  className,
-  onQuit,
-}: {
-  isShow: boolean;
-  onSelected: () => void;
-  className?: string;
+export default function MenuStack(props: {
   /** ゲームをやめるコールバック */
   onQuit?: () => void;
+  id: string;
 }) {
   const [variant, setVariant] = useAtom(atomOfAnswerImageVariant);
   const [initial] = useAtom(atomOfInitial);
   const [game] = useAtom(atomOfGame);
-  const onSelectNum = useCallback(
-    () => (setVariant('num'), onSelected()),
-    [onSelected, setVariant],
-  );
-  const onSelectIcon = useCallback(
-    () => (setVariant('asobi'), onSelected()),
-    [onSelected, setVariant],
-  );
-  const numClass = useMemo(
-    () =>
-      variant === 'num' ? 'svg-checked dark:bg-zinc-800' : 'svg-unchecked',
-    [variant],
-  );
-  const iconClass = useMemo(
-    () => (variant === 'asobi' ? 'svg-checked' : 'svg-unchecked'),
-    [variant],
-  );
-  const containerClass = useMemo(
-    () =>
-      clsx(
-        className,
-        'shadow border-zinc-500 rounded p-2 bg-zinc-50 dark:bg-zinc-700 flex-col',
-      ),
-    [className],
-  );
   const QuitButton = memo(() =>
-    !initial || !game ? null : <Quit onQuit={onQuit} onCancel={onSelected} />,
+    !initial || !game ? null : <Quit onQuit={props.onQuit} />,
   );
+
+  const createUrl = useCallback(() => {
+    if (!initial || !game) {
+      return;
+    }
+    const params = toURLSearchParam({
+      puzzle: initial,
+      blockSize: game.blockSize,
+      cross: game.cross,
+      hyper: game.hyper,
+    });
+    const url = `${location.origin}/?${params.toString()}`;
+    console.log(url);
+    return url;
+  }, [initial, game]);
+
   const ShareButton = memo(() => {
     if (!initial || !game) {
       return null;
     }
     if (initial && game && navigator.canShare && navigator.share) {
       return (
-        <Button
-          className="block text-xl w-full text-left"
-          variant="text"
+        <button
           onClick={() => {
-            const params = toURLSearchParam({
-              puzzle: initial,
-              blockSize: game.blockSize,
-              cross: game.cross,
-              hyper: game.hyper,
-            });
-            const url = `${location.origin}/?${params.toString()}`;
-            console.log(url);
+            const url = createUrl();
+            if (!url) return;
             if (navigator.canShare({ url })) {
               navigator.share({ url });
             }
-            onSelected();
           }}
         >
           シェアする
-        </Button>
+        </button>
       );
     } else {
       return (
-        <Button
-          className="block text-xl w-full text-left"
-          variant="text"
+        <button
           onClick={() => {
-            const params = toURLSearchParam({
-              puzzle: initial,
-              blockSize: game.blockSize,
-              cross: game.cross,
-              hyper: game.hyper,
-            });
-            const url = `${location.origin}/?${params.toString()}`;
-            console.log(url);
+            const url = createUrl();
+            if (!url) return;
             navigator.clipboard
               .writeText(url)
               .then(() => alert('コピーしました'));
-            onSelected();
           }}
         >
           問題のURLをコピー
-        </Button>
+        </button>
       );
     }
   });
   return (
-    <div
-      className={containerClass}
-      style={{ display: isShow ? 'flex' : 'none' }}
-    >
-      <QuitButton />
-      <ShareButton />
-      <Button
-        className="block text-xl w-full text-left"
-        variant="text"
-        onClick={onSelectNum}
-      >
-        <span className={numClass}></span>数字で遊ぶ
-      </Button>
-      <Button
-        className="block text-xl w-full text-left"
-        variant="text"
-        onClick={onSelectIcon}
-      >
-        <span className={iconClass}></span>アイコンで遊ぶ
-      </Button>
+    <div className="popover-menu" id={props.id} popover="auto">
+      <ul className={styles.menu}>
+        <li>
+          <QuitButton />
+        </li>
+        <li>
+          <ShareButton />
+        </li>
+        <li>
+          <button onClick={() => setVariant('num')}>
+            <span className={getIconClass(variant, 'num')}></span>
+            数字で遊ぶ
+          </button>
+        </li>
+        <li>
+          <button onClick={() => setVariant('asobi')}>
+            <span className={getIconClass(variant, 'icon')}></span>
+            アイコンで遊ぶ
+          </button>
+        </li>
+      </ul>
     </div>
   );
+}
+
+function getIconClass(variant: 'num' | 'asobi', target: 'icon' | 'num') {
+  if (target === 'num') {
+    return variant === 'num' ? styles.svgChecked : styles.svgUnchecked;
+  } else {
+    return variant === 'asobi' ? styles.svgChecked : styles.svgUnchecked;
+  }
 }
