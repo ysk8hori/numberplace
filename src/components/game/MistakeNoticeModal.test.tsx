1eb2333ae2c20fb14fda1d835c94bf371e70034d
ysk8hori <ysk8.hori@gmail.com>
2025-11-09 15:25:13 +0900

    refactor: MistakeNoticeModal から react-modal を剥がす

--- src/components/game/MistakeNoticeModal.test.tsx
+++ src/components/game/MistakeNoticeModal.test.tsx
@@ -4,7 +4,7 @@ import { render, screen } from '../../utils/test-utils';
 import MistakeNoticeModal from './MistakeNoticeModal';
 
 test('モーダルに「間違いがあります」を表示する', async () => {
-  render(<MistakeNoticeModal />);
+  render(<MistakeNoticeModal hasMistake={true} />);
   expect(
     screen.queryByRole('dialog', { name: '不正解です' }),
   ).toHaveTextContent('間違いがあります');
--- src/components/game/MistakeNoticeModal.tsx
+++ src/components/game/MistakeNoticeModal.tsx
@@ -1,6 +1,7 @@
-import React, { useCallback, useState } from 'react';
-import Modal from '../atoms/Modal';
+import React, { useEffect } from 'react';
 import Button from '../atoms/Button';
+
+const MISTAKE_NOTICE_MODAL_ID = 'mistake-notice-modal';
 
 /**
  * 誤答がある旨を知らせるモーダル
@@ -8,24 +9,34 @@ import Button from '../atoms/Button';
  * - 誤答がある場合はその旨の通知を表示する
  */
 const MistakeNoticeModal: React.FC<{
-  /** OKボタン押下 */
-  onOk?: () => void;
-}> = ({ onOk }) => {
-  const [isOpen, setOpenState] = useState(true);
-  const close = useCallback(() => setOpenState(false), [setOpenState]);
+  /** OKボタン押下などでミスがある旨をクリアする */
+  clearMistake?: () => void;
+  hasMistake: boolean;
+}> = ({ clearMistake, hasMistake }) => {
+  useEffect(() => {
+    const popover = document.getElementById(MISTAKE_NOTICE_MODAL_ID);
+    if (popover) {
+      if (hasMistake) {
+        popover.showPopover();
+      }
+    }
+    clearMistake?.();
+  }, [hasMistake]);
   return (
-    <Modal isOpen={!!isOpen} contentLabel="不正解です">
+    <div id={MISTAKE_NOTICE_MODAL_ID} popover="auto">
       <p className="text-center">間違いがあります</p>
       <div className="flex justify-center">
         <Button
           variant="text"
-          onClick={() => (close(), onOk?.())}
+          onClick={() => {
+            document.getElementById(MISTAKE_NOTICE_MODAL_ID)?.hidePopover();
+          }}
           className="w-full"
         >
           OK
         </Button>
       </div>
-    </Modal>
+    </div>
   );
 };
 export default MistakeNoticeModal;
